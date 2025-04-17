63dc741e2608696c7d2200396337012bf0ab8097
akbar.ashraf <itsakbar.ashraf@gmail.com>
2025-04-17 15:36:13 +0500

    sorting added for save searches

--- src/app/[locale]/(protected)/[roles]/dashboard/(customer)/saved-searches/components/components/saved-searches-table.tsx
+++ src/app/[locale]/(protected)/[roles]/dashboard/(customer)/saved-searches/components/components/saved-searches-table.tsx
@@ -1,14 +1,36 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { DataTable } from "@/components/dataTable/data-table";
 import type { SortingState } from "@tanstack/react-table";
 
 import { SavedSearchesColumns } from "./saved-searches-columns";
-import { ISavedSearches } from "@/types/protected/properties";
+import { ISavedSearch, ISavedSearches } from "@/types/protected/properties";
 
 export default function SavedSearchesTable(data: ISavedSearches) {
   const [sorting, setSorting] = useState<SortingState>([]);
+
+  const getSortedData = useMemo(() => {
+    if (sorting.length > 0) {
+      const { id: sortField, desc } = sorting[0];
+
+      return [...data.results].sort((a, b) => {
+        const aValue = a[sortField as keyof ISavedSearch[]];
+        const bValue = b[sortField as keyof ISavedSearch[]];
+
+        if (typeof aValue === "string" && typeof bValue === "string") {
+          return desc
+            ? bValue.localeCompare(aValue)
+            : aValue.localeCompare(bValue);
+        }
+
+        if (aValue < bValue) return desc ? 1 : -1;
+        if (aValue > bValue) return desc ? -1 : 1;
+        return 0;
+      });
+    }
+    return data.results;
+  }, [data, sorting]);
 
   return (
     <>
@@ -20,7 +42,7 @@ export default function SavedSearchesTable(data: ISavedSearches) {
 
           <DataTable
             columns={SavedSearchesColumns}
-            data={data?.results || []}
+            data={getSortedData || []}
             sorting={sorting}
             onSortingChange={setSorting}
           />
